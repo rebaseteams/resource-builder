c2786aebb087cd5e0e10ac31c07b50084878d5eb
saritChauhan <saritsinghchauhan@gmail.com>
2022-02-02 12:40:23 +0530

    generic interface and class

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,20 +1,52 @@
-import { createConnection, Repository } from "typeorm";
+import { Connection, Repository } from "typeorm";
 import { GenericInterface } from "../interfaces";
 
 export class GenericRepo<T> implements GenericInterface<T>{
     private repository: Repository<T>
+    private type: string
 
-    constructor(type: string) {
-        createConnection().then((connection) => {
-          this.repository = connection.getRepository(type);
-        });
+    constructor(connection: Connection, type: string) {
+      this.repository = connection.getRepository(type);
+      this.type = type
       }
+        async create(data: T): Promise<T> {
+          const resp = await this.repository.save(data)
+          if(resp) return data
+          const err = { message: `Cannot create ${this.type}`, statusCode: 404 };
+          throw err;
+        }
 
         async findOne(id: string): Promise<T> {
-            const item = await this.repository.findOne(id);
-            if (item) return item;
-            const err = { message: `Resource not found for id: ${id}`, statusCode: 404 };
-            throw err;
+          const item = await this.repository.findOne(id) as T;
+          if(item) return item;
+          const err = { message: `${this.type} not found for id: ${id}`, statusCode: 404 };
+          throw err;
+        }
+
+        async find(skip : number, limit : number): Promise<T[]> {
+          const items: T[] = await this.repository.find({
+            take: limit,
+            skip,
+          });
+          if(items.length) return items;
+          const err = { message: `${this.type}s not found`, statusCode: 404 };
+          throw err;
+        }
+
+        async update(data: T): Promise<{ success: boolean }> {
+          const resp = await this.repository.save(data)
+          if(resp) return { success: true }
+          const err = { message: `Cannot update ${this.type}`, statusCode: 404 };
+          throw err;
+        }
+
+        async delete(id: string): Promise<{success: boolean}> {
+          const resp =this.repository.delete(id);
+          const affected = (await resp).affected
+          if(affected && affected > 0)
+          return { success: true };
+          const err = { message: `Cannot delete ${this.type} for id: ${id}`, statusCode: 404 };
+          throw err;
         }
 
 }
