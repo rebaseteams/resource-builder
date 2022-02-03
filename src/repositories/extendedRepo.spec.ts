d19a34c3e8b0a95865665d78eaebf1b45dca31e7
saritChauhan <saritsinghchauhan@gmail.com>
2022-02-03 16:49:38 +0530

    unit tests for extended branch

--- src/repositories/extendedRepo.spec.ts
+++ src/repositories/extendedRepo.spec.ts
@@ -24,6 +24,22 @@ describe('ExtendedTypeORMRepo', ()=>{
         create(data: TestResource): Promise<TestResource | Error> {
             return super.create(data)
         }
+
+        findOne(id: string): Promise<TestResource | Error> {
+            return super.findOne(id)
+        }
+
+        find(skip: number, limit: number): Promise<Error | TestResource[]> {
+            return super.find(skip, limit)
+        }
+
+        update(data: TestResource): Promise<Error | { success: boolean; }> {
+            return super.update(data)
+        }
+
+        delete(id: string): Promise<Error | { success: boolean; }> {
+            return super.delete(id)
+        }
     }
 
     const setup = async (): Promise<{ repo: RepoInterface<TestResource>, connection: Connection }> => {
@@ -68,4 +84,120 @@ describe('ExtendedTypeORMRepo', ()=>{
             await connection.close();
         });
     });
+
+    describe('findOne', ()=>{
+
+        it('should throw error when invalid id is passed', async () => {
+            const { repo, connection } = await setup()
+            const actual = await repo.findOne('');
+            const expected: Error = {
+                name: 'Invalid Id Error',
+                message: 'Id passed is not valid',
+            };
+            expect(actual).toStrictEqual(expected);
+            await connection.close();
+        });
+
+        it('should throw error when nothing found', async () => {
+            const { repo, connection } = await setup()
+            const actual = await repo.findOne('a');
+            const expected: Error = {
+                name: 'Find resource by Id Error',
+                message: 'TestResourceEntity not found for id: a',
+            };
+            expect(actual).toStrictEqual(expected);
+            await connection.close();
+        });
+
+        it('should sucessfully find the resource for given id', async () => {
+            const { repo, connection } = await setup()
+            const data: TestResource = { id: '1', name: 'nns', description: 'ddd' }
+            await repo.create(data);
+            const actual = await repo.findOne('1');
+            expect(actual).toEqual(data);
+            await connection.close();
+        })
+    })
+
+    describe('find', ()=>{
+
+        it('should throw error when invalid skip or limit is passed', async () => {
+            const { repo, connection } = await setup()
+            const actual = await repo.find(null as unknown as number, null as unknown as number);
+            const expected: Error = {
+                name: 'Invalid query Error',
+                message: 'queries passed are not valid',
+            };
+            expect(actual).toStrictEqual(expected);
+            await connection.close();
+        });
+
+        it('should sucessfully find the resources', async () => {
+            const { repo, connection } = await setup()
+            const data: TestResource = { id: '1', name: 'nns', description: 'ddd' }
+            await repo.create(data);
+            const actual = await repo.find(0, 1);
+            expect(actual).toEqual([data]);
+            await connection.close();
+        })
+    })
+
+    describe('update', ()=>{
+
+        it('should successfully update the resource', async () => {
+            const { repo, connection } = await setup()
+            const orignalData: TestResource = { id: '1', name: 'nn', description: 'dd44' }
+            const newData: TestResource = { id: '1', name: 'aa', description: 'aaa' }
+            await repo.create(orignalData);
+            const actual = await repo.update(newData)
+            expect(actual).toStrictEqual({success: true});
+            await connection.close();
+        });
+
+        it('should throw error when typeORM fails to persists', async () => {
+            const { repo, connection } = await setup()
+            const actual = await repo.update({} as TestResourceEntity);
+            const expected: Error = {
+                name: 'Update Resource Error',
+                message: 'Error occured while updating TestResourceEntity',
+                stack: 'QueryFailedError: SQLITE_CONSTRAINT: NOT NULL constraint failed: test_resource_entity.id'
+            };
+            expect(actual).toStrictEqual(expected);
+            await connection.close();
+        });
+    })
+
+    describe('delete', ()=>{
+
+        it('should throw error when invalid id is passed', async () => {
+            const { repo, connection } = await setup()
+            const actual = await repo.delete('');
+            const expected: Error = {
+                name: 'Invalid Id Error',
+                message: 'Id passed is not valid',
+            };
+            expect(actual).toStrictEqual(expected);
+            await connection.close();
+        });
+
+        it('should successfully delete the resource', async () => {
+            const { repo, connection } = await setup()
+            const data: TestResource = { id: '1', name: 'nn', description: 'dd44' }
+            await repo.create(data);
+            const actual = await repo.delete('1')
+            expect(actual).toStrictEqual({success: true});
+            await connection.close();
+        });
+
+        it('should throw error when unregistered id is passed', async () => {
+            const { repo, connection } = await setup()
+            const actual = await repo.delete('ssss');
+            const expected: Error = {
+                name: 'Delete Resource Error',
+                message: 'Error occured while deleting TestResourceEntity for id: ssss',
+            };
+            expect(actual).toStrictEqual(expected);
+            await connection.close();
+        });
+    })
 })
