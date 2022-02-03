41240f5a7a630a88dcc5ad3809c08b1055867a6e
Rebase Dev <rebase.teams@gmail.com>
2022-02-03 08:50:13 +0530

    updated create tests

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,7 @@
 export interface RepoInterface<T>{
-    create(data: T) : Promise<T>;
-    findOne(id: string): Promise<T>;
-    find(skip : number, limit : number): Promise<T[]>;
+    create(data: T) : Promise<T | Error>;
+    findOne(id: string): Promise<T | Error>;
+    find(skip : number, limit : number): Promise<T[] | Error>;
     update(data: T) : Promise<{success: boolean}>;
     delete(id: string): Promise<{success: boolean}>;
 }
