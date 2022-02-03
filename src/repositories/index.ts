8bc090d5026579b66d2ebb3d9ed9fc1b3020cd49
saritChauhan <saritsinghchauhan@gmail.com>
2022-02-03 10:25:02 +0530

    fixed minor changes

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Connection, Repository } from "typeorm";
 import { RepoInterface } from "../interfaces";
 
@@ -9,6 +10,15 @@ export class BaseTypeORMRepo<T> implements RepoInterface<T>{
       this.repository = connection.getRepository(resourceName);
       this.resourceName = resourceName
       }
+        find(skip: number, limit: number): Promise<Error | T[]> {
+          throw new Error("Method not implemented.");
+        }
+        update(data: T): Promise<{ success: boolean; }> {
+          throw new Error("Method not implemented.");
+        }
+        delete(id: string): Promise<{ success: boolean; }> {
+          throw new Error("Method not implemented.");
+        }
         async create(data: T): Promise<T | Error> {
           let resp: Awaited<T>;
           try {
@@ -25,37 +35,56 @@ export class BaseTypeORMRepo<T> implements RepoInterface<T>{
           
         }
 
-        async findOne(id: string): Promise<T> {
-          const item = await this.repository.findOne(id) as T;
-          if(item) return item;
-          const err = { message: `${this.resourceName} not found for id: ${id}` };
-          throw err;
+        async findOne(id: string): Promise<T | Error> {
+          if(!id){
+            const err: Error = {
+              name: 'Find resource by Id Error',
+              message: `${this.resourceName} not found for id: ${id}`,
+            };
+            return err
+          }
+          try {
+            const item = await this.repository.findOne(id) as T;
+            if(item) {return item;}
+            const err: Error = {
+              name: 'Find resource by Id Error',
+              message: `${this.resourceName} not found for id: ${id}`,
+            };
+            return err
+          } catch (e) {
+            const err: Error = {
+              name: 'Find resource by Id Error',
+              message: `${this.resourceName} not found for id: ${id}`,
+              stack: (e as Error).toString()
+            };
+            return err
+          }
         }
 
-        async find(skip : number, limit : number): Promise<T[]> {
-          const items: T[] = await this.repository.find({
-            take: limit,
-            skip,
-          });
-          if(items.length) return items;
-          const err = { message: `${this.resourceName}s not found` };
-          throw err;
-        }
+        // async find(skip : number, limit : number): Promise<T[]> {
+        //   const items: T[] = await this.repository.find({
+        //     take: limit,
+        //     skip,
+        //   });
+        //   if(items.length) return items;
+        //   const err = { message: `${this.resourceName}s not found` };
+        //   throw err;
+        // }
 
-        async update(data: T): Promise<{ success: boolean }> {
-          const resp = await this.repository.save(data)
-          if(resp) return { success: true }
-          const err = { message: `Cannot update ${this.resourceName}` };
-          throw err;
-        }
+        // async update(data: T): Promise<{ success: boolean }> {
+        //   const resp = await this.repository.save(data)
+        //   if(resp) return { success: true }
+        //   const err = { message: `Cannot update ${this.resourceName}` };
+        //   throw err;
+        // }
 
-        async delete(id: string): Promise<{success: boolean}> {
-          const resp =this.repository.delete(id);
-          const affected = (await resp).affected
-          if(affected && affected > 0)
-          return { success: true };
-          const err = { message: `Cannot delete ${this.resourceName} for id: ${id}` };
-          throw err;
-        }
+        // async delete(id: string): Promise<{success: boolean}> {
+        //   const resp =this.repository.delete(id);
+        //   const affected = (await resp).affected
+        //   if(affected && affected > 0)
+        //   return { success: true };
+        //   const err = { message: `Cannot delete ${this.resourceName} for id: ${id}` };
+        //   throw err;
+        // }
 
 }
